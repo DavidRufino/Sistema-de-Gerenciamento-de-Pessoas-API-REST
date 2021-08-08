56fafedfa428ddd6d82afbeb29198dee4c56bf60
DavidRufino <rufino.inbox@gmail.com>
2021-08-08 17:24:22 -0300

    Adicionado mais Test Unity

--- src/test/java/one/digitalinnovation/personapi/service/PersonServiceTest.java
+++ src/test/java/one/digitalinnovation/personapi/service/PersonServiceTest.java
@@ -1,19 +1,39 @@
 package one.digitalinnovation.personapi.service;
 
+import one.digitalinnovation.personapi.controller.PersonController;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import java.util.Collections;
+import java.util.List;
 
 import static one.digitalinnovation.personapi.utils.PersonUtils.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+
+import static org.hamcrest.core.Is.is;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonServiceTest {
@@ -36,6 +56,12 @@ public class PersonServiceTest {
     @InjectMocks
     private PersonService personService;
 
+    //private static final String PEOPLE_API_URL_PATH = "http://localhost:8080/api/v1/people";
+
+    //private MockMvc mockMvc;
+
+    //private PersonController personController;
+
     @Test
     void testGivenPersonDTOThenReturnSavedMessage() {
         PersonDTO personDTO = createFakeDTO();
@@ -44,7 +70,6 @@ public class PersonServiceTest {
         //  Quando for chamar um method save dentro da class PersonService.java
         //  Passando o objeto esperado 'expectedSavedPerson'
         //  Me retorne a mensagem (mensagem criada com sucesso ou nao)
-        //when(personRepository.save(expectedSavedPerson)).thenReturn(expectedSavedPerson);
         when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
 
         MessageResponseDTO expectedSucessMessage = createdExpectedMessageResponse("Created Person with ID: ", expectedSavedPerson.getId());
@@ -52,6 +77,101 @@ public class PersonServiceTest {
 
         assertEquals(expectedSucessMessage, succesMessage);
     }
+
+    //  Adicionado mais Test Unity
+    /*@BeforeEach
+    void setUp() {
+        personController = new PersonController(personService);
+        mockMvc = MockMvcBuilders.standaloneSetup(personController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setViewResolvers((viewName, locale) -> new MappingJackson2JsonView())
+                .build();
+    }
+
+    @Test
+    void testWhenPOSTIsCalledThenAPersonShouldBeCreated() throws Exception {
+        PersonDTO expectedPersonDTO = createFakeDTO();
+        MessageResponseDTO expectedResponseMessage = createdExpectedMessageResponse("Created Person with ID: ", 1L);
+
+        when(personService.createPerson(expectedPersonDTO)).thenReturn(expectedResponseMessage);
+
+        mockMvc.perform(post(PEOPLE_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(expectedPersonDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message", is(expectedResponseMessage.getMessage())));
+    }
+
+    @Test
+    void testWhenGETWithValidIsCalledThenAPersonShouldBeReturned() throws Exception {
+        var expectedValidId = 1L;
+        PersonDTO expectedPersonDTO = createFakeDTO();
+        expectedPersonDTO.setId(expectedValidId);
+
+        when(personService.findById(expectedValidId)).thenReturn(expectedPersonDTO);
+
+        mockMvc.perform(get(PEOPLE_API_URL_PATH + "/" + expectedValidId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.firstName", is("Rodrigo")))
+                .andExpect(jsonPath("$.lastName", is("Peleias")));
+    }
+
+    @Test
+    void testWhenGETWithInvalidIsCalledThenAnErrorMessagenShouldBeReturned() throws Exception {
+        var expectedValidId = 1L;
+        PersonDTO expectedPersonDTO = createFakeDTO();
+        expectedPersonDTO.setId(expectedValidId);
+
+        when(personService.findById(expectedValidId)).thenThrow(PersonNotFoundException.class);
+
+        mockMvc.perform(get(PEOPLE_API_URL_PATH + "/" + expectedValidId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testWhenGETIsCalledThenPeopleListShouldBeReturned() throws Exception {
+        var expectedValidId = 1L;
+        PersonDTO expectedPersonDTO = createFakeDTO();
+        expectedPersonDTO.setId(expectedValidId);
+        List<PersonDTO> expectedPeopleDTOList = Collections.singletonList(expectedPersonDTO);
+
+        when(personService.listAll()).thenReturn(expectedPeopleDTOList);
+
+        mockMvc.perform(get(PEOPLE_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].firstName", is("Rodrigo")))
+                .andExpect(jsonPath("$[0].lastName", is("Peleias")));
+    }
+
+    @Test
+    void testWhenPUTIsCalledThenAPersonShouldBeUpdated() throws Exception {
+        var expectedValidId = 1L;
+        PersonDTO expectedPersonDTO = createFakeDTO();
+        MessageResponseDTO expectedResponseMessage = createdExpectedMessageResponse("Updated Person with ID: ", 1L);
+
+        when(personService.updateById(expectedValidId, expectedPersonDTO)).thenReturn(expectedResponseMessage);
+
+        mockMvc.perform(put(PEOPLE_API_URL_PATH + "/" + expectedValidId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(expectedPersonDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", is(expectedResponseMessage.getMessage())));
+    }
+
+    @Test
+    void testWhenDELETEIsCalledThenAPersonShouldBeDeleted() throws Exception {
+        var expectedValidId = 1L;
+
+        mockMvc.perform(delete(PEOPLE_API_URL_PATH + "/" + expectedValidId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+    */
 
     public MessageResponseDTO createdExpectedMessageResponse(String Message, Long idPeople) {
         return MessageResponseDTO
