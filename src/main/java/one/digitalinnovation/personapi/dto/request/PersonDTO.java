76cf0611013328d7f4b775c8ce4417db3b8dddc6
DavidRufino <rufino.inbox@gmail.com>
2021-08-07 23:43:58 -0300

    Improved Person creation functionality

---
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -0,0 +1,42 @@
+package one.digitalinnovation.personapi.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.personapi.entity.Phone;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO {
+
+    private Long id;    //  Atributo ID
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String firstName;   //  Atributo First_Name
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String lastName;    //  Atributo Last_Name
+
+    @NotEmpty
+    @CPF
+    private String cpf; //  Atributo CPF
+
+    private String birthDate;    //  Atributo Birth_Date
+
+    @Valid
+    @NotEmpty
+    private List<Phone> phones; //  Atributo Phones
+}
---
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PhoneDTO.java
@@ -0,0 +1,34 @@
+package one.digitalinnovation.personapi.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.personapi.enums.PhoneType;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhoneDTO {
+
+    /*
+    *   Um 'Clone' da entidade Person em DTO - Data transfer object
+    *   PersonDTO sera um objeto para fazer a transferencia de dados,
+    *   ele sera o objeto responsavel por receber todas as entradas de dados
+    * */
+
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PhoneType type;
+
+    @NotEmpty
+    @Size(min = 13, max = 14)
+    private String number;
+}
